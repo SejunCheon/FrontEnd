1b9bd93330f7ffdaebd4925b1f14464b0fda32f9
SejunCheon <kjsug1030@naver.com>
2021-07-22 19:14:22 +0900

    Add Vue TodoList

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>My Todo App!</h1>
+    <TodoList></TodoList>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  -webkit-font-something: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #32485f;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
+
---
+++ hello-vue/src/components/BaseInputText.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" class="input" :value="value" v-on="listeners" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: String, default: "" },
+  },
+  computed: {
+    listeners() {
+      return {
+        // 자식에게서 부모에게 전달되는 모든 이벤트들
+        ...this.$listeners,
+        // input 이벤트만 재정의
+        input: (event) => this.$emit("input", event.target.value),
+      }; // 각 항목에 체크박스달고 체크한 시간표시도 해보기
+    },
+  },
+};
+</script>
+<style scoped>
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid #32485f;
+}
+</style>
